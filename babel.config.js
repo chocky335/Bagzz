8f9075d3241e382954d38b6d316fb6250ebe5c0a
Bohdan Pomohaibo <b.pomohaibo@globallogic.com>
2022-11-19 00:49:43 +0200

    feat: configure relative imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          $src: './src',
+          $features: './src/features',
+          $i18n: './src/i18n',
+          $kit: './src/kit',
+          $navigation: './src/navigation',
+          $services: './src/services',
+          $store: './src/store',
+          $theme: './src/theme',
+        },
+      },
+    ],
+  ],
 };
