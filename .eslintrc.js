f0670831460bf5b79dc204a5ff44b2a25406f5df
Bohdan Pomohaibo <b.pomohaibo@globallogic.com>
2022-11-19 01:01:44 +0200

    feat: configure import sorting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'i18next'],
+  plugins: ['@typescript-eslint', 'i18next', 'simple-import-sort', 'import'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -11,6 +11,38 @@ module.exports = {
         'no-shadow': 'off',
         'no-undef': 'off',
         'i18next/no-literal-string': 'error',
+
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // type imports.
+              ['\\u0000$'],
+              // packages: `react` related packages come first
+              ['^react', 'react', '^@?\\w'],
+              // side effect imports.
+              ['^\\u0000'],
+              // $src imports
+              ['^\\$src'],
+              ['^\\$features'],
+              ['^\\$i18n'],
+              ['^\\$kit'],
+              ['^\\$navigation'],
+              ['^\\$services'],
+              ['^\\$store'],
+              ['^\\$theme'],
+              // relative imports: parents, children, same-folder  `..` and `.` last
+              [
+                '^\\.\\.(?!/?$)',
+                '^\\.\\./?$',
+                '^\\./(?=.*/)(?!/?$)',
+                '^\\.(?!/?$)',
+                '^\\./?$',
+              ],
+            ],
+          },
+        ],
+        'import/no-duplicates': 'error',
       },
     },
   ],
